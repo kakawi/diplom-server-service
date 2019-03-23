699966e43ffc4cd2b3f3da4f07b9181a5e4c9411
Hleb Bandarenka <hleb.bandarenka@gmail.com>
2019-03-23 19:50:26 +0300

    campaign find by id

--- src/main/java/com/hlebon/adsweb/service/CampaignService.java
+++ src/main/java/com/hlebon/adsweb/service/CampaignService.java
@@ -3,7 +3,6 @@ package com.hlebon.adsweb.service;
 import com.hlebon.adsweb.repository.dao.CampaignDao;
 import com.hlebon.adsweb.repository.dao.CampaignStatisticDao;
 import com.hlebon.adsweb.repository.entity.CampaignEntity;
-import com.hlebon.adsweb.repository.entity.CampaignStatisticEntity;
 import com.hlebon.adsweb.service.mapper.CampaignMapper;
 import com.hlebon.adsweb.service.mapper.CampaignStatisticMapper;
 import com.hlebon.adsweb.service.searchable.CampaignSearchableObject;
@@ -17,10 +16,10 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 import java.util.TreeSet;
 import java.util.stream.Collectors;
-
-import static java.util.Arrays.asList;
 
 @Service
 @RequiredArgsConstructor
@@ -31,41 +30,39 @@ public class CampaignService {
     private final CampaignMapper campaignMapper;
     private final CampaignStatisticMapper campaignStatisticMapper;
 
-    private List<CampaignDto> campaignDtos = asList(
-    );
-
     @Transactional(readOnly = true)
-    public List<CampaignEntity> findAll() {
-        final List<CampaignEntity> result = campaignDao.findAll();
-
-        return result;
-    }
-
     public List<CampaignDto> findAll(final CampaignSearchableObject campaignSearchableObject) {
         final List<CampaignEntity> allCampaigns = campaignDao.findAll();
         final List<CampaignDto> result = allCampaigns.stream().map(campaignMapper::mapToDto).collect(Collectors.toList());
         if (campaignSearchableObject.isFetchStatistics()) {
-            fillStatistics(result);
+            result.forEach(c -> c.setCampaignStatistics(fillStatistics(c)));
         }
         return result;
     }
 
-    private void fillStatistics(final List<CampaignDto> campaignDtos) {
-        final Pageable pageable = PageRequest.of(0, 6, Sort.by("year").descending().and(Sort.by("weekNumber").descending()));
-        for (final CampaignDto campaign : campaignDtos) {
-            final List<CampaignStatisticEntity> campaignStatistics = campaignStatisticDao.findByCampaign_Id(campaign.getId(), pageable);
-            campaign.setCampaignStatistics(campaignStatistics
-                    .stream()
-                    .map(campaignStatisticMapper::mapToDto)
-                    .collect(Collectors.toCollection(
-                            () -> new TreeSet<>(Comparator.comparing(CampaignDto.CampaignStatisticDto::getWeekNumber)))
-                    )
-            );
+    @Transactional(readOnly = true)
+    public CampaignDto findById(final long id, final CampaignSearchableObject campaignSearchableObject) {
+        final Optional<CampaignEntity> campaign = campaignDao.findById(id);
+        if (!campaign.isPresent()) {
+            return null;
         }
+        final CampaignEntity campaignEntity = campaign.get();
+        final CampaignDto result = campaignMapper.mapToDto(campaignEntity);
+        if (campaignSearchableObject.isFetchStatistics()) {
+            result.setCampaignStatistics(fillStatistics(result));
+        }
+        return result;
     }
 
-    public CampaignDto findById(final long id) {
-        return campaignDtos.stream().filter(c -> c.getId() == id).findFirst().get();
+    private Set<CampaignDto.CampaignStatisticDto> fillStatistics(final CampaignDto campaignDto) {
+        final Pageable pageable = PageRequest.of(0, 6, Sort.by("year").descending().and(Sort.by("weekNumber").descending()));
+        return campaignStatisticDao
+                .findByCampaign_Id(campaignDto.getId(), pageable)
+                .stream()
+                .map(campaignStatisticMapper::mapToDto)
+                .collect(Collectors.toCollection(
+                        () -> new TreeSet<>(Comparator.comparing(CampaignDto.CampaignStatisticDto::getWeekNumber)))
+                );
     }
 
 }
